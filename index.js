b1d4e3346aa2612e54bf78b959e3509c41b94400
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-09-05 19:19:08 +1000

    js event listners

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.getElementById("searchInput");
+    const searchButton = document.getElementById("searchButton");
+    const filterButton = document.getElementsByClassName(".filterButton");
+});
