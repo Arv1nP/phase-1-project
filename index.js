b433f348ee546104bdf5d2e8947a3d740457f889
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-09-05 19:22:33 +1000

    console.log test

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("click", () => {
     const searchInput = document.getElementById("searchInput");
     const searchButton = document.getElementById("searchButton");
     const filterButton = document.getElementsByClassName(".filterButton");
+    
+    console.log("Search Input:", searchInput);
+    console.log("Search Button:", searchButton);
+    console.log("Filter Buttons:", filterButton);
 });
