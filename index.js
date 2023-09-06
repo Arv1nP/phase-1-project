1e4f2ed5ce1370a4cb57f3c561ce55b4b9efda6a
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-09-06 11:41:47 +1000

    lost at sea

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const searchInput = document.getElementsByClassName("search-input");
-const searchButton = document.getElementsByClassName("search-button");
+const searchInput = document.querySelector(".search-input");
+const userSearchInput = searchInput.value.toLowerCase();
+const searchButton = document.querySelector(".search-button");
 const resultsContainer = document.getElementById("results-section");
 
-searchButton.addEventListener("click", () => {
-    const userSearch = searchInput.value;
-    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(userSearch)}`;
+
+async function fetchResults(){
+searchButton.addEventListener(click, () => {
+    const userSearch = userSearchInput;
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${userSearch}`;
     fetch(apiUrl)
         .then((dataF) => console.log(dataF.json()))
+
         .catch((error) => {
-            console.error("Error could not load:", error);
+            console.error("Book not found", error);
         });
-});
+    });
+}
+fetchResults()
