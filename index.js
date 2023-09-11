d60fac289947acaefba5c803fa6d892435bd1a3e
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-09-11 16:57:42 +1000

    search bar basic

--- index.js
+++ index.js
@@ -1,19 +1,81 @@
-const searchInput = document.querySelector(".search-input");
-const userSearchInput = searchInput.value.toLowerCase();
-const searchButton = document.querySelector(".search-button");
-const resultsContainer = document.getElementById("results-section");
+//DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function () {
+    const googleBooksKey = "AIzaSyB80K-o0NisdXXsA9BUG5ZrwUFar3eYeuU";
+
+    const searchInput = document.querySelector(".search-input");
+    const searchButton = document.querySelector(".searchButton");
+    const resultsContainer = document.getElementById("book-list");
+    const filterButton = document.getElementById("filter-button");
+    const filterOptions = document.getElementById("filter-options")
+
+    async function fetchResultsThumbnail() {
+        const userSearchInput = searchInput.value.toLowerCase();
+        const apiLink = `https://www.googleapis.com/books/v1/volumes?q=${userSearchInput}&maxResults=5&key=${googleBooksKey}`;
+
+        try {
+            const response = await fetch(apiLink);
+            if (!response.ok) {
+                throw new Error(`Network had an issue ${Error}`);}
+
+            const data = await response.json();
+
+            resultsContainer.innerHTML = "";
+
+            data.items.forEach((item) => {
+                const bookInfo = item.volumeInfo;
+
+                const li = document.createElement("li");
+                const title = document.createElement("h3");
+                const author = document.createElement("p");
+                const rating = document.createElement("p");
+                const thumbnail = document.createElement("img");
+
+                title.textContent = bookInfo.title;
+                author.textContent = `Author: ${bookInfo.authors ? bookInfo.authors.join(", ") : "Unknown"}`;
+                rating.textContent = `Rating: ${bookInfo.averageRating || "N/A"}`;
+                thumbnail.src = bookInfo.imageLinks ? bookInfo.imageLinks.thumbnail : "";
+
+                li.appendChild(thumbnail);
+                li.appendChild(title);
+                li.appendChild(author);
+                li.appendChild(rating);
+                resultsContainer.appendChild(li)
+            });
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    searchButton.addEventListener("click", fetchResultsThumbnail);
+    searchInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            fetchResultsThumbnail()}});
+
+    async function filterbtn(){
+        const userSearchInput = searchInput.value.toLowerCase()
+        const apiLink = `https://www.googleapis.com/books/v1/volumes?q=${userSearchInput}&maxResults=4&key=${googleBooksKey}`;
+        try{
+            const response = await fetch(apiLink);
+            if (!response.ok){
+                throw new Error(`Network had an issue ${Error}`);}
+
+                const data = await response.json();
+
+                const li1 = document.createElement("li");
+                const li2 = document.createElement("li");
+                const li3 = document.createElement("li");
+
+                const sortyByDate = 1
+                const sortByRating = 2
+                const sortByPages = 3
+                const sortByAZ = 4
+
+    filterButton.addEventListener("click", filterbtn)
 
 
-async function fetchResults(){
-searchButton.addEventListener(click, () => {
-    const userSearch = userSearchInput;
-    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${userSearch}`;
-    fetch(apiUrl)
-        .then((dataF) => console.log(dataF.json()))
-
-        .catch((error) => {
-            console.error("Book not found", error);
-        });
-    });
-}
-fetchResults()
+        }catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+    
+});
